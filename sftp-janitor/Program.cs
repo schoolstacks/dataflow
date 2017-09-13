83a1265671fcabcf342f3bd291c3e2a915d1e258
Jason Hoekstra <jasonhoekstra@gmail.com>
2017-09-13 00:43:35 -0400

    basic SFTP functionality

--- sftp-janitor/Program.cs
+++ sftp-janitor/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
 
 namespace sftp_janitor
 {
@@ -10,6 +10,45 @@ namespace sftp_janitor
     {
         static void Main(string[] args)
         {
+            string host;
+            string username;
+            string password;
+
+            host = "example.com";
+            username = "user";
+            password = "pass";
+
+            try
+            {
+                SftpClient client = new SftpClient(host, username, password);
+                client.Connect();
+                Console.WriteLine(client.ConnectionInfo.ServerVersion);
+
+                IEnumerable<SftpFile> fileList = client.ListDirectory("**directory**");
+                foreach (SftpFile file in fileList)
+                {
+                    if (file.Name.Contains("**mask**"))
+                    {
+                        Console.WriteLine(file.FullName);
+                        string filename = @"**directory**" + file.Name;
+                        Stream fileStream = File.OpenWrite(filename);
+                        client.DownloadFile(file.FullName, fileStream);
+                        fileStream.Close();
+
+                    }
+
+                    
+                }
+
+                client.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
         }
     }
 }
