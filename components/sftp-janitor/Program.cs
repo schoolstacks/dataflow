af6dffdacee56bd9379855e02e455caddd66b8ab
Eduardo Fonseca <efonsecab@gmail.com>
2017-09-20 21:24:49 +0000

    Adding initial draft to parse csv in order to transform them

--- components/sftp-janitor/Program.cs
+++ components/sftp-janitor/Program.cs
@@ -15,7 +15,6 @@ namespace sftp_janitor
         static void Main(string[] args)
         {
             //TestsFTP();
-            //ListFiles();
             WriteLocalFilesToAzureFileStorage();
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
@@ -58,51 +57,6 @@ namespace sftp_janitor
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
-            }
-        }
-
-        private static void ListFiles()
-        {
-            //Check https://docs.microsoft.com/en-us/azure/storage/files/storage-dotnet-how-to-use-files
-            // Parse the connection string and return a reference to the storage account.
-            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-            //    CloudConfigurationManager.GetSetting("StorageConnectionString"));
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                Properties.Settings.Default.StorageConnectionString);
-            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
-            CloudFileShare fileShare = fileClient.GetShareReference(Properties.Settings.Default.FileShareName);
-            CloudFileDirectory fileDirectoryRoot = fileShare.GetRootDirectoryReference();
-            var allItems = fileDirectoryRoot.ListFilesAndDirectories();
-            var allRootDirectories = allItems.OfType<CloudFileDirectory>();
-            var allRootFiles = allItems.OfType<CloudFile>();
-            foreach (CloudFileDirectory singleDirectory in allRootDirectories)
-            {
-                Console.WriteLine(singleDirectory.Uri.ToString());
-                var currentDirectoryFiles = singleDirectory.ListFilesAndDirectories().OfType<CloudFile>();
-                foreach (var singleFile in currentDirectoryFiles)
-                {
-                    Console.WriteLine(singleFile.Uri);
-                    string dir = Properties.Settings.Default.LocalDestinationDirectory;
-                    if (!Directory.Exists(dir))
-                        Directory.CreateDirectory(dir);
-                    string localFile = System.IO.Path.Combine(dir, singleFile.Parent.Name, singleFile.Name);
-                    var localDir = Path.GetDirectoryName(localFile);
-                    if (!Directory.Exists(localDir))
-                        Directory.CreateDirectory(localDir);
-                    singleFile.DownloadToFile(localFile, FileMode.Create);
-                }
-            }
-            foreach (CloudFile singleFile in allRootFiles)
-            {
-                Console.WriteLine(singleFile.Uri.ToString());
-                using (var strReader = singleFile.OpenRead())
-                {
-                    string strFileContent = singleFile.DownloadText();
-                    Console.WriteLine("**********START OF {0}**********", singleFile.Name);
-                    Console.WriteLine(strFileContent);
-                    Console.WriteLine("**********END OF {0}**********", singleFile.Name);
-                    strReader.Close();
-                }
             }
         }
 
--- components/transform-api-load-janitor/Program.cs
+++ components/transform-api-load-janitor/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,99 @@ namespace transform_api_load_janitor
     {
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            TransformFilesFromAzureFileStorage();
+            watch.Stop();
+            Console.WriteLine("Time Elapsed: {0}", watch.Elapsed.ToString());
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("An unhandled exception has occurred: " + ((Exception)e.ExceptionObject).ToString());
+        }
+
+        private static void TransformFile(string filePath)
+        {
+            using (System.IO.StreamReader textReader = new System.IO.StreamReader(filePath))
+            {
+                using (CsvHelper.CsvReader csvReader = new CsvHelper.CsvReader(textReader))
+                {
+                    bool isFirstRow = true;
+                    int rowNumber = 0;
+                    while (csvReader.Read())
+                    {
+                        Console.WriteLine("Start of Data for Row: {0}", rowNumber);
+                        for (int iCol = 0; iCol < csvReader.CurrentRecord.Length; iCol++)
+                        {
+                            string columnName = csvReader.FieldHeaders[iCol];
+                            string columnValue = csvReader.GetField(iCol);
+                            Console.WriteLine("{0} = {1}", columnName, columnValue);
+                        }
+                        Console.WriteLine("End of Data for Row: {0}", rowNumber);
+                        rowNumber++;
+                    }
+                }
+            }
+        }
+
+        private static void TransformFilesFromAzureFileStorage()
+        {
+            //Check https://docs.microsoft.com/en-us/azure/storage/files/storage-dotnet-how-to-use-files
+            // Parse the connection string and return a reference to the storage account.
+            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+            //    CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                Properties.Settings.Default.StorageConnectionString);
+            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+            CloudFileShare fileShare = fileClient.GetShareReference(Properties.Settings.Default.FileShareName);
+            CloudFileDirectory fileDirectoryRoot = fileShare.GetRootDirectoryReference();
+            var allItems = fileDirectoryRoot.ListFilesAndDirectories();
+            var allRootDirectories = allItems.OfType<CloudFileDirectory>();
+            var allRootFiles = allItems.OfType<CloudFile>();
+            foreach (CloudFileDirectory singleDirectory in allRootDirectories)
+            {
+                Console.WriteLine(singleDirectory.Uri.ToString());
+                var currentDirectoryFiles = singleDirectory.ListFilesAndDirectories().OfType<CloudFile>();
+                foreach (var singleFile in currentDirectoryFiles)
+                {
+                    var extension = Path.GetExtension(singleFile.Name).ToUpper();
+                    if (extension != ".CSV")
+                        continue;
+                    Console.WriteLine(singleFile.Uri);
+                    string dir = Properties.Settings.Default.LocalDestinationDirectory;
+                    if (!Directory.Exists(dir))
+                        Directory.CreateDirectory(dir);
+                    string localFile = System.IO.Path.Combine(dir, singleFile.Parent.Name, singleFile.Name);
+                    var localDir = Path.GetDirectoryName(localFile);
+                    if (!Directory.Exists(localDir))
+                        Directory.CreateDirectory(localDir);
+                    singleFile.DownloadToFile(localFile, FileMode.Create);
+                    try
+                    {
+                        TransformFile(localFile);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+            foreach (CloudFile singleFile in allRootFiles)
+            {
+                Console.WriteLine(singleFile.Uri.ToString());
+                using (var strReader = singleFile.OpenRead())
+                {
+                    string strFileContent = singleFile.DownloadText();
+                    Console.WriteLine("**********START OF {0}**********", singleFile.Name);
+                    Console.WriteLine(strFileContent);
+                    Console.WriteLine("**********END OF {0}**********", singleFile.Name);
+                    strReader.Close();
+                }
+            }
         }
     }
 }
