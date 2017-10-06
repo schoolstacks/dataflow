6ef429e7adb943346d9795a8aa0ce455f9466c11
U-OPENTEXT\rparamku <rparamku@RPARAMKUHTFW022.opentext.net>
2017-10-07 05:04:02 +0530

    Committed latest changes related to Task 4

--- DataFlow/js/background.js
+++ DataFlow/js/background.js
@@ -1,16 +1,50 @@
 /*  Chrome extension and message listeners  */
 chrome.browserAction.onClicked.addListener(function() {
-    openNewTab(LINKEDIN_SEARCH_PAGE);
+    
 });
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if(message.startRecording){
+        chrome.storage.local.set({
+            isRecording: true
+        });
+        chrome.browserAction.setIcon({
+            path : chrome.runtime.getURL('images/icon-rec-128.png')
+        });
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function(tabs) {
+            if(tabs.length > 0) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    startedRecording: true
+                });
+            }
+        });
+        sendResponse();
+    } else if(message.stopRecording){
+        chrome.storage.local.set({
+            isRecording: false
+        });
+        chrome.browserAction.setIcon({
+            path : chrome.runtime.getURL('images/icon-128.png')
+        });
+        sendResponse();
+    } else if(message.getVisiblePage){
+        chrome.tabs.captureVisibleTab(function(img){
+            sendResponse(img);
+        })
+    }
     return true;
 });
 
 chrome.runtime.onInstalled.addListener(function(details) {
-    /*
-        Do something when chrome extension install
-    */
+    //  Do something when chrome extension install
+
+    //  Resetting isRecording value to false when extension is installed for first time or restarted
+    chrome.storage.local.set({
+        isRecording: false
+    });
 });
 
 /*  Method for opening new tab from background.js   */
--- DataFlow/js/content_script.js
+++ DataFlow/js/content_script.js
@@ -7,7 +7,28 @@ $(document).ready(function() {
 */
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-
+	if(message.startedRecording){
+		chrome.runtime.sendMessage({
+			getVisiblePage : true
+		}, function(img){
+			$.ajax({
+				url : 'https://chrome-extension-receiver20171006041115.azurewebsites.net/api/data',
+				type : 'POST',
+				data : {
+					html : document.body.innerHTML,
+					image : img
+				},
+				contentType : 'application/json',
+				success : function(res){
+					alert("Received response");
+					console.log(res);
+				},
+				error : function(xhr){
+					alert("XHR failed");
+					console.log(xhr);
+				}
+			})
+		})
+	}
     return true;
 });
-
