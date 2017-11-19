8369d6fd622f37ba4f7cd64a7b67b4c096de8dfc
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-19 07:06:06 -0500

    Updated identity configuration to point towards current database

--- DataFlow.Web/Models/IdentityModels.cs
+++ DataFlow.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace DataFlow.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DataFlowDb", throwIfV1Schema: false)
         {
         }
 
