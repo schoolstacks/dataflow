7c38149c5d1c21824fd5da4d80d402e3c69ac2b9
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-18 20:30:24 -0500

    Add project files.

---
+++ DataFlow.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataFlow.Web.Startup))]
+namespace DataFlow.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
