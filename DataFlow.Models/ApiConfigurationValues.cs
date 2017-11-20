4db12c2ab23f56886b3cf53e3dab0c01229eaa7b
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-20 17:44:52 -0500

    Added EdFi Service to MVC App. Added Schools page.

---
+++ DataFlow.Models/ApiConfigurationValues.cs
@@ -0,0 +1,15 @@
+﻿namespace DataFlow.Models
+{
+    public class ApiConfigurationValues
+    {
+        public string API_SERVER_URL { get; set; }
+        public string API_SERVER_KEY { get; set; }
+        public string API_SERVER_SECRET { get; set; }
+        public string DEFAULTS_TERM_MONTH { get; set; }
+        public string DEFAULTS_TERM_YEAR { get; set; }
+        public string INSTANCE_COMPANY_NAME { get; set; }
+        public string INSTANCE_COMPANY_LOGO { get; set; }
+        public string INSTANCE_COMPANY_URL { get; set; }
+        public string INSTANCE_EDU_USE_TEXT { get; set; }
+    }
+}
--- DataFlow.Web/Controllers/SchoolController.cs
+++ DataFlow.Web/Controllers/SchoolController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataFlow.Common.DAL;
+using DataFlow.Web.Helpers;
+using DataFlow.Web.Models;
+using DataFlow.Web.Services;
 
 namespace DataFlow.Web.Controllers
 {
-    public class SchoolController : Controller
+    public class SchoolController : BaseController
     {
+        private DataFlowDbContext dataFlowDbContext;
+        private EdFiService edFiService;
+
+        public SchoolController(DataFlowDbContext dataFlowDbContext, EdFiService edFiService)
+        {
+            this.dataFlowDbContext = dataFlowDbContext;
+            this.edFiService = edFiService;
+        }
+
         // GET: School
         public ActionResult Index()
         {
-            return View();
+            var schools = edFiService.GetSchools(0, 50);
+            var schoolGrid = new List<SchoolViewModel.Grid>();
+            schools.ForEach(x =>
+            {
+                var school = new SchoolViewModel.Grid()
+                {
+                    Id = x.id,
+                    Name = x.nameOfInstitution,
+                    Abbreviation = x.shortNameOfInstitution
+                };
+                schoolGrid.Add(school);
+            });
+
+            return View(schoolGrid);
         }
     }
 }
