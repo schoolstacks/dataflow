af55416baa3504ee853df4bb6fbc0d3cf9ee9b85
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-21 14:05:46 -0500

    Create Lookups page

---
+++ DataFlow.Web/Controllers/LookupController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataFlow.Common.DAL;
+using DataFlow.Web.Helpers;
+using DataFlow.Web.Services;
+
+namespace DataFlow.Web.Controllers
+{
+    public class LookupController : BaseController
+    {
+        private readonly DataFlowDbContext dataFlowDbContext;
+        private readonly EdFiService edFiService;
+
+        public LookupController(DataFlowDbContext dataFlowDbContext, EdFiService edFiService)
+        {
+            this.dataFlowDbContext = dataFlowDbContext;
+            this.edFiService = edFiService;
+        }
+
+        public ActionResult Index()
+        {
+            var vm = dataFlowDbContext.Lookups
+                .OrderBy(x => x.GroupSet)
+                .ThenBy(x => x.Key)
+                .ToList();
+
+            return View(vm);
+        }
+    }
+}
