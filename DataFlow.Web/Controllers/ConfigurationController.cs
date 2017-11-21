979722e4784e06d2c44ae9151b01ec0e89480aa6
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-21 15:16:36 -0500

    WIP Beginning work on Configuration page

---
+++ DataFlow.Web/Controllers/ConfigurationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataFlow.Common.DAL;
+using DataFlow.Web.Services;
+
+namespace DataFlow.Web.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        private readonly DataFlowDbContext dataFlowDbContext;
+        private readonly EdFiService edFiService;
+
+        public ConfigurationController(DataFlowDbContext dataFlowDbContext, EdFiService edFiService)
+        {
+            this.dataFlowDbContext = dataFlowDbContext;
+            this.edFiService = edFiService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
