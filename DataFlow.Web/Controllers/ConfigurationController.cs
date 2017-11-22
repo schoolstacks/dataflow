445ad57c56400f3911e98bafec66d7c8a030af6b
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-21 20:30:21 -0500

    Configuration page with test and update functionality.

--- DataFlow.Web/Controllers/ConfigurationController.cs
+++ DataFlow.Web/Controllers/ConfigurationController.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataFlow.Common;
 using DataFlow.Common.DAL;
+using DataFlow.EdFi.Api.Resources;
+using DataFlow.EdFi.Sdk;
+using DataFlow.Web.Helpers;
 using DataFlow.Web.Services;
+using RestSharp;
 
 namespace DataFlow.Web.Controllers
 {
@@ -21,7 +26,77 @@ namespace DataFlow.Web.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var vm = edFiService.GetConfiguration();
+
+            ViewBag.Months = new SelectList(Helpers.Common.MonthSelectList(), "Value", "Text");
+            ViewBag.Years = new SelectList(Helpers.Common.YearSelectList(), "Value", "Text");
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public bool ConfigurationIsOk(string apiServerUrl, string apiServerKey, string apiServerSecret)
+        {
+            try
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = (senderLocal, certificate, chain, sslPolicyErrors) => true;
+
+                var oAuthUrl = Helpers.Common.GetUntilOrEmpty(apiServerUrl.Trim(), "/api/");
+
+                var client = new RestClient(apiServerUrl);
+                var tokenRetriever = new TokenRetriever(oAuthUrl, apiServerKey, apiServerSecret);
+                client.Authenticator = new BearerTokenAuthenticator(tokenRetriever);
+
+                var api = new SchoolsApi(client);
+                var apiCall = api.GetSchoolsAll(0, 1);
+
+                return apiCall.IsSuccessful;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update(DataFlow.Models.ApiConfigurationValues updatedValues)
+        {
+            var apiServerUrl = edFiService.GetConfigurationByKey(Constants.API_SERVER_URL);
+            var apiServerKey = edFiService.GetConfigurationByKey(Constants.API_SERVER_KEY);
+            var apiServerSecret = edFiService.GetConfigurationByKey(Constants.API_SERVER_SECRET);
+            var defaultsTermMonth = edFiService.GetConfigurationByKey(Constants.DEFAULTS_TERM_MONTH);
+            var defaultsTermYear = edFiService.GetConfigurationByKey(Constants.DEFAULTS_TERM_YEAR);
+            var instanceCompanyName = edFiService.GetConfigurationByKey(Constants.INSTANCE_COMPANY_NAME);
+            var instanceCompanyLogo = edFiService.GetConfigurationByKey(Constants.INSTANCE_COMPANY_LOGO);
+            var instanceCompanyUrl = edFiService.GetConfigurationByKey(Constants.INSTANCE_COMPANY_URL);
+            var instanceEduUseText = edFiService.GetConfigurationByKey(Constants.INSTANCE_EDU_USE_TEXT);
+
+            apiServerUrl.Value = updatedValues.API_SERVER_URL;
+            apiServerKey.Value = updatedValues.API_SERVER_KEY;
+            apiServerSecret.Value = updatedValues.API_SERVER_SECRET;
+            defaultsTermMonth.Value = updatedValues.DEFAULTS_TERM_MONTH;
+            defaultsTermYear.Value = updatedValues.DEFAULTS_TERM_YEAR;
+            instanceCompanyName.Value = updatedValues.INSTANCE_COMPANY_NAME;
+            instanceCompanyLogo.Value = updatedValues.INSTANCE_COMPANY_LOGO;
+            instanceCompanyUrl.Value = updatedValues.INSTANCE_COMPANY_URL;
+            instanceEduUseText.Value = updatedValues.INSTANCE_EDU_USE_TEXT;
+
+            var confs = new List<DataFlow.Models.Configuration>
+            {
+                apiServerUrl,
+                apiServerKey,
+                apiServerSecret,
+                defaultsTermMonth,
+                defaultsTermYear,
+                instanceCompanyName,
+                instanceCompanyLogo,
+                instanceCompanyUrl,
+                instanceEduUseText
+            };
+
+            edFiService.SaveConfiguration(confs);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- DataFlow.Web/Helpers/Common.cs
+++ DataFlow.Web/Helpers/Common.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace DataFlow.Web.Helpers
 {
@@ -20,6 +21,34 @@ namespace DataFlow.Web.Helpers
             }
 
             return string.Empty;
+        }
+
+        public static List<SelectListItem> MonthSelectList()
+        {
+            var monthList = new List<SelectListItem>();
+            monthList.Add(new SelectListItem() { Text = "Select Month", Value = string.Empty });
+            monthList.AddRange(Enumerable.Range(01, 12).Select(x =>
+                new SelectListItem()
+                {
+                    Text = x.ToString().PadLeft(2, '0'),
+                    Value = x.ToString().PadLeft(2, '0')
+                }));
+
+            return monthList;
+        }
+
+        public static List<SelectListItem> YearSelectList()
+        {
+            var yearList = new List<SelectListItem>();
+            yearList.Add(new SelectListItem() { Text = "Select Year", Value = string.Empty });
+            yearList.AddRange(Enumerable.Range(2010, 12).Select(x =>
+                new SelectListItem()
+                {
+                    Text = x.ToString(),
+                    Value = x.ToString()
+                }));
+
+            return yearList;
         }
     }
 }
