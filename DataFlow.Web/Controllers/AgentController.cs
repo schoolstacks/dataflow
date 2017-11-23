db61bbfaf3e5fae6107e3a55c55a0857c5ab508e
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-22 19:56:46 -0500

    Began work on manual upload

--- DataFlow.Web/Controllers/AgentController.cs
+++ DataFlow.Web/Controllers/AgentController.cs
@@ -27,7 +27,31 @@ namespace DataFlow.Web.Controllers
                 .Include(x=>x.File)
                 .ToList();
 
+            ViewBag.Agents = GetAgentList;
+
             return View(agents);
+        }
+
+        public ActionResult UploadFile()
+        {
+            return RedirectToAction("Index");
+        }
+
+        private List<SelectListItem> GetAgentList
+        {
+            get
+            {
+                var entityList = new List<SelectListItem>();
+                entityList.Add(new SelectListItem() { Text = "Select Agent", Value = string.Empty });
+                entityList.AddRange(dataFlowDbContext.Agents.Select(x =>
+                    new SelectListItem()
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString()
+                    }));
+
+                return entityList;
+            }
         }
     }
 }
