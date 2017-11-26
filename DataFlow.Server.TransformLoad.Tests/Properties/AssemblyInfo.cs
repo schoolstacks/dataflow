a5a12d4385663aff7a826c8a7c60eba40e8f6841
Eduardo Fonseca <efonsecab@gmail.com>
2017-11-26 16:33:06 +0000

    adding unit tests POC for transformation service

--- DataFlow.Server.TransformLoad.Tests/Properties/AssemblyInfo.cs
+++ DataFlow.Server.TransformLoad.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataFlow.Server.TransformLoad")]
+[assembly: AssemblyTitle("DataFlow.Server.TransformLoad.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataFlow.Server.TransformLoad")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("DataFlow.Server.TransformLoad.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d0365edb-aec3-49da-89ad-6fcffe586dcc")]
+[assembly: Guid("863850e2-b8b6-4605-9e36-987cbc3f8fd7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("DataFlow.Server.TransformLoad.Tests")]
