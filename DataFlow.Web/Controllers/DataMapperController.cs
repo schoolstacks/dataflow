3e08d35c1ec0aabc0a0cfad57f850f2d1673b9c1
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-27 13:38:57 -0500

    WIP Advanced Data Mapper

---
+++ DataFlow.Web/Controllers/DataMapperController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataFlow.Common.DAL;
+using DataFlow.Web.Helpers;
+using DataFlow.Web.Services;
+
+namespace DataFlow.Web.Controllers
+{
+    public class DataMapperController : BaseController
+    {
+        private readonly DataFlowDbContext dataFlowDbContext;
+        private readonly EdFiService edFiService;
+
+        public DataMapperController(DataFlowDbContext dataFlowDbContext, EdFiService edFiService)
+        {
+            this.dataFlowDbContext = dataFlowDbContext;
+            this.edFiService = edFiService;
+        }
+
+        // GET: DataMapper
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private List<SelectListItem> GetEntityList
+        {
+            get
+            {
+                var entityList = new List<SelectListItem>();
+                entityList.Add(new SelectListItem() { Text = "Select Entity", Value = string.Empty });
+                entityList.AddRange(dataFlowDbContext.Entities.Select(x =>
+                    new SelectListItem()
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString()
+                    }));
+
+                return entityList;
+            }
+        }
+    }
+}
