6f2530f534951fdd3a3019ad1131c63a2a2932ff
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-27 17:07:15 -0500

    Continued work on Advanced Data Mapper

---
+++ DataFlow.Common/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DataFlow.Common.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string NullIfWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value) ? null : value;
+        }
+    }
+}
