654f5f1c36c1ea36857444ec89ca0adc507d1825
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-27 08:51:56 -0500

    Added Assessment Index and updated Navigation

--- DataFlow.Web/Models/AssessmentViewModel.cs
+++ DataFlow.Web/Models/AssessmentViewModel.cs
@@ -8,9 +8,12 @@ namespace DataFlow.Web.Models
     {
         public class AddOrEdit
         {
-            public DataFlow.EdFi.Models.Resources.Assessment Assessment { get; set; }
-            public DataFlow.EdFi.Models.Resources.AssessmentIdentificationCode IdentificationCode { get; set; }
-            public DataFlow.EdFi.Models.Resources.ObjectiveAssessment ObjectiveAssessment { get; set; }
+            public EdFi.Models.Resources.Assessment Assessment { get; set; }
+            public EdFi.Models.Resources.AssessmentIdentificationCode IdentificationCode { get; set; }
+
+            [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter object assessment(s) separated by new lines.")]
+            [Display(Name = "Objective Assessments")]
+            public string ObjectiveAssessmentsText { get; set; }
 
             [Display(Name = "Grade Levels")]
             public List<CheckBox> GradeLevels { get; set; }
