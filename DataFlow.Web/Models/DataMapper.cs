edf607604f47c1b1c226f54248f84ea1471dfdb6
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-28 13:38:30 -0500

    Generating dynamic JSON

---
+++ DataFlow.Web/Models/DataMapper.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace DataFlow.Web.Models
+{
+    public class DataMapper
+    {
+        public string Name { get; set; }
+
+        public DataMapperProperty DataMapperProperty { get; set; }
+    }
+
+    public class DataMapperProperty
+    {
+        [JsonProperty(PropertyName = "data-type")]
+        public string DataType { get; set; }
+
+        [JsonProperty(PropertyName = "source")]
+        public string Source { get; set; }
+
+        [JsonProperty(PropertyName = "source-column")]
+        public string SourceColumn { get; set; }
+
+        [JsonProperty(PropertyName = "source-table", NullValueHandling = NullValueHandling.Ignore)]
+        public string SourceTable { get; set; }
+
+        [JsonProperty(PropertyName = "default", NullValueHandling = NullValueHandling.Ignore)]
+        public string Default { get; set; }
+
+        [JsonProperty(PropertyName = "value", NullValueHandling = NullValueHandling.Ignore)]
+        public string Value { get; set; }
+    }
+}
