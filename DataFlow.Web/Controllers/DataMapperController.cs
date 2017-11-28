576b4e51a7ceaab02b54febe5b46ea00e9df8731
AJ Tatum <andrew.j.tatum@gmail.com>
2017-11-28 13:38:30 -0500

    Generating dynamic JSON

--- DataFlow.Web/Controllers/DataMapperController.cs
+++ DataFlow.Web/Controllers/DataMapperController.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using DataFlow.Common.DAL;
+using DataFlow.Common.ExtensionMethods;
 using DataFlow.Common.Services;
 using DataFlow.Web.Helpers;
+using DataFlow.Web.Models;
 using DataFlow.Web.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DataFlow.Web.Controllers
 {
@@ -26,9 +28,9 @@ namespace DataFlow.Web.Controllers
 
         public ActionResult Index()
         {
-            var vm = new Models.DataMapperViewModel();
+            var vm = new DataMapperViewModel();
             vm.Entities = GetEntityList;
-            vm.Fields = new List<string>()
+            vm.Fields = new List<string>
             {
                 "FieldOne",
                 "FieldTwo",
@@ -39,9 +41,41 @@ namespace DataFlow.Web.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(Models.DataMapperViewModel vm)
+        public ActionResult Index(DataMapperViewModel vm)
         {
             return View(vm);
+        }
+
+        [HttpPost]
+        public JsonResult UpdateJsonMap(FormCollection formCollection)
+        {
+            var fields = formCollection["FieldNames"].Split(';').ToList();
+
+            var jObject = new JObject();
+
+            fields.ForEach(f =>
+            {
+                var model = new DataMapper
+                {
+                    Name = f,
+                    DataMapperProperty = new DataMapperProperty
+                    {
+                        Source = formCollection[$"txt{f}_SourceType"].NullIfWhiteSpace(),
+                        SourceColumn = formCollection[$"txt{f}_SourceColumn"].NullIfWhiteSpace(),
+                        DataType = "string",
+                        Default = formCollection[$"txt{f}_DefaultValue"].NullIfWhiteSpace(),
+                        SourceTable = formCollection[$"txt{f}_SourceTable"].NullIfWhiteSpace(),
+                        Value = formCollection[$"txt{f}_StaticValue"].NullIfWhiteSpace()
+                    }
+                };
+
+                jObject.Add(model.Name, JObject.FromObject(model.DataMapperProperty));
+            });
+
+            var cleanJson = JsonHelper.RemoveEmptyChildren(jObject);
+            var jsonMap = cleanJson.ToString(Formatting.Indented);
+
+            return Json(jsonMap);
         }
 
         private List<SelectListItem> GetEntityList
@@ -49,11 +83,11 @@ namespace DataFlow.Web.Controllers
             get
             {
                 var entityList = new List<SelectListItem>();
-                entityList.Add(new SelectListItem() { Text = "Select Entity", Value = string.Empty });
+                entityList.Add(new SelectListItem { Text = "Select Entity", Value = string.Empty });
                 entityList.AddRange(dataFlowDbContext.Entities
                     .OrderBy(x => x.Name)
                     .Select(x =>
-                        new SelectListItem()
+                        new SelectListItem
                         {
                             Text = x.Name,
                             Value = x.Id.ToString()
