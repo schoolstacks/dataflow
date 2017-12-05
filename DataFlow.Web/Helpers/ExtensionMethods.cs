8b901884f7cf56c1f4c547521b06e29a0e7fe717
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-05 17:17:00 -0500

    Attempted to wrap up adding multiple rows

---
+++ DataFlow.Web/Helpers/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataFlow.Web.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static string SplitGetElementAt(this string text, char splitCharacter, int elementAt, bool returnNullIfEmtpty = true)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return returnNullIfEmtpty ? null : text;
+
+            var returnValue = text.Split(splitCharacter).ElementAtOrDefault(elementAt);
+
+            if (string.IsNullOrWhiteSpace(returnValue))
+                return returnNullIfEmtpty ? null : returnValue;
+
+            return returnValue;
+        }
+    }
+}
