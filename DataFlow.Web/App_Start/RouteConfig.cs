094e3397b5ebfa5ae82416a17fb67b7cb6069370
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-06 09:49:56 -0500

    Updated routes so that the default route is to the School Index. Removed Home from Nav.

--- DataFlow.Web/App_Start/RouteConfig.cs
+++ DataFlow.Web/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace DataFlow.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "School", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
