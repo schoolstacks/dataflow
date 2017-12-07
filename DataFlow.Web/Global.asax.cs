0e229986826f4377fefad63ed2993c4fa8748c06
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-07 17:32:22 -0500

    Add BootstrapData screens with edit/delete and ensuring that the ProcessOrder would remain entact.

--- DataFlow.Web/Global.asax.cs
+++ DataFlow.Web/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -18,13 +19,15 @@ namespace DataFlow.Web
     {
         protected void Application_Start()
         {
-            var cacheConfig = ConfigurationBuilder.BuildConfiguration("DataFlow.Web", settings =>
+            var appName = ConfigurationManager.AppSettings["AppName"] ?? string.Empty;
+
+            var cacheConfig = ConfigurationBuilder.BuildConfiguration(appName, settings =>
             {
                 settings.WithUpdateMode(CacheUpdateMode.None)
-                    .WithSystemRuntimeCacheHandle("DataFlow.Web")
+                    .WithSystemRuntimeCacheHandle(appName)
                     .WithExpiration(ExpirationMode.Sliding, TimeSpan.FromHours(2));
             });
-            var cacheFactory = CacheFactory.FromConfiguration<string>("DataFlow.Web", cacheConfig);
+            var cacheFactory = CacheFactory.FromConfiguration<string>(appName, cacheConfig);
 
             var builder = new ContainerBuilder();
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
@@ -34,22 +37,15 @@ namespace DataFlow.Web
             builder.RegisterType<EdFiService>().InstancePerRequest();
             builder.RegisterType<ConfigurationService>().InstancePerRequest();
             builder.RegisterType<EdFiMetadataProcessor>().InstancePerRequest();
-            builder.Register(c => LogManager.GetLogger("DataFlow.Web")).As<ILogger>().InstancePerRequest();
+            builder.Register(c => LogManager.GetLogger(appName)).As<ILogger>().InstancePerRequest();
             builder.RegisterType<NLogService>().AsImplementedInterfaces().InstancePerRequest();
+            builder.RegisterType<WebConfigAppSettingsService>().AsImplementedInterfaces().InstancePerRequest();
 
             builder.RegisterType<CacheService>().WithParameters(new[]
             {
                 new TypedParameter(typeof(ICacheManagerConfiguration), cacheConfig),
                 new TypedParameter(typeof(ICacheManager<string>), cacheFactory)
             }).As<ICacheService>().SingleInstance();
-
-            //builder.Register(c =>
-            //{
-            //    var result = new BaseController();
-            //    var dep = c.Resolve<ConfigurationService>();
-            //    result.ConfigurationService = dep;
-            //    return result;
-            //});
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
--- DataFlow.Web/Helpers/JsonHelper.cs
+++ DataFlow.Web/Helpers/JsonHelper.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
 
 namespace DataFlow.Web.Helpers
 {
@@ -46,6 +49,37 @@ namespace DataFlow.Web.Helpers
         public static bool IsEmpty(JToken token)
         {
             return (token.Type == JTokenType.Null);
+        }
+
+        public static bool IsValidJson(string strInput)
+        {
+            if (string.IsNullOrWhiteSpace(strInput))
+                return false;
+
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+                (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    var obj = JToken.Parse(strInput);
+                    return true;
+                }
+                catch (JsonReaderException jex)
+                {
+                    LogManager.GetCurrentClassLogger().Log(LogLevel.Error, jex);
+                    return false;
+                }
+                catch (Exception ex) //some other exception
+                {
+                    LogManager.GetCurrentClassLogger().Log(LogLevel.Error, ex);
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
