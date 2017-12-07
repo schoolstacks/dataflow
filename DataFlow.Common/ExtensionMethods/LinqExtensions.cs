a273d945466d44e2ee530e5a8d0b160ccd9564a4
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-07 17:32:22 -0500

    Add BootstrapData screens with edit/delete and ensuring that the ProcessOrder would remain entact.

---
+++ DataFlow.Common/ExtensionMethods/LinqExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataFlow.Common.ExtensionMethods
+{
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// If the condition is true, it performs the where
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, bool>> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        /// <summary>
+        /// If the condition is true, it performs the where
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, int, bool>> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        /// <summary>
+        /// If the condition is true, it performs the where
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> WhereIf<TSource>(this IEnumerable<TSource> source, bool condition, Func<TSource, bool> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        /// <summary>
+        /// If the condition is true, it performs the where
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> WhereIf<TSource>(this IEnumerable<TSource> source, bool condition, Func<TSource, int, bool> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        public static IQueryable<TSource> Between<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>> keySelector, TKey low, TKey high) where TKey : IComparable<TKey>
+        {
+            if (keySelector != null)
+            {
+                Expression key = Expression.Invoke(keySelector, keySelector.Parameters.ToArray());
+                Expression lowerBound = Expression.GreaterThanOrEqual(key, Expression.Constant(low));
+                Expression upperBound = Expression.LessThanOrEqual(key, Expression.Constant(high));
+                Expression and = Expression.AndAlso(lowerBound, upperBound);
+                Expression<Func<TSource, bool>> lambda = Expression.Lambda<Func<TSource, bool>>(and, keySelector.Parameters);
+                return source.Where(lambda);
+            }
+            return null;
+        }
+    }
+}
