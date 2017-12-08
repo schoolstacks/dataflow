34c281b3e9c18136852b09959255a78e2f8776e1
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-08 05:59:02 -0500

    Added interfaces to make unit testing easier. Created simple Cache page to view cache stats and clear the cache if needed.

---
+++ DataFlow.Web/Services/BaseServices.cs
@@ -0,0 +1,11 @@
+﻿using DataFlow.Common.Services;
+
+namespace DataFlow.Web.Services
+{
+    public class BaseServices : IBaseServices
+    {
+        public ICentralLogger Logger { get; }
+        public ICacheService CacheService { get; }
+        public IConfigurationService ConfigurationService { get; }
+    }
+}
---
+++ DataFlow.Web/Services/IConfigurationService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using DataFlow.Models;
+
+namespace DataFlow.Web.Services
+{
+    public interface IConfigurationService
+    {
+        List<Configuration> GetConfigurationFromCache();
+        Configuration GetConfigurationByKey(string key);
+        void SaveConfiguration(List<Configuration> confs);
+        ApiConfigurationValues GetConfiguration();
+    }
+}
