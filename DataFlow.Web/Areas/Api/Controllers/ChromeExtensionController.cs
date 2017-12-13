4b4e2864e767648f63fed9cb33247b0bfec1b8c7
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-13 10:00:59 -0500

    Initial WebApi implementation

---
+++ DataFlow.Web/Areas/Api/Controllers/ChromeExtensionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DataFlow.Web.Areas.Api.Controllers
+{
+    [AllowAnonymous]
+    public class ChromeExtensionController : ApiController
+    {
+        [HttpGet]
+        [Route("api/register")]
+        public HttpResponseMessage Register()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Test"),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
