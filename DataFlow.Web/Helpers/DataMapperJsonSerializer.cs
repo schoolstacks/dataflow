87492a333c65e6891d50c302ddb86b5abd95b909
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-13 15:23:59 -0500

    WIP: Alternate route by using a custom JsonSerializer

---
+++ DataFlow.Web/Helpers/DataMapperJsonSerializer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataFlow.Models;
+using DataFlow.Web.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DataFlow.Web.Helpers
+{
+    public class DataMapperJsonSerializer : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            try
+            {
+                if (value is DataMapper dataMapper)
+                {
+                    if (dataMapper.SubDataMappers.Any())
+                    {
+                        writer.WriteStartObject();
+                        writer.WritePropertyName(dataMapper.Name);
+                        writer.WriteStartArray();
+                        //
+                        dataMapper.SubDataMappers.ForEach(sub =>
+                        {
+                            writer.WriteStartObject();
+                            writer.WritePropertyName(sub.Name);
+                            writer.WriteStartObject();
+                            sub.SubDataMappers.ForEach(tri =>
+                            {
+                                writer.WriteStartObject();
+                                writer.WritePropertyName(tri.Name);
+                                writer.WriteStartObject();
+                                serializer.Serialize(writer, tri.DataMapperProperty);
+                                writer.WriteEndObject();
+                            });
+                            //serializer.Serialize(writer, sub.DataMapperProperty);
+                            writer.WriteEndObject();
+                        });
+                        //
+                        writer.WriteEndArray();
+                        writer.WriteEndObject();
+                    }
+                    else
+                    {
+                        writer.WriteStartObject();
+                        writer.WritePropertyName(dataMapper.Name);
+                        serializer.Serialize(writer, dataMapper.DataMapperProperty);
+                        writer.WriteEndObject();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("Error");
+                serializer.Serialize(writer, e);
+                writer.WriteEndObject();
+            }
+            
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jsonObject = JObject.Load(reader);
+            var properties = jsonObject.Properties().ToList();
+            return new DataMapper()
+            {
+                Name = properties[0].Name,
+                DataMapperProperty = (DataMapperProperty)properties[0].Value
+            };
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(DataMapper).IsAssignableFrom(objectType);
+        }
+    }
+}
