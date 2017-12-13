d5065d5fef670f8e6b25555c785639c51e2742c9
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-13 10:00:59 -0500

    Initial WebApi implementation

---
+++ DataFlow.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace DataFlow.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Attribute routing.
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
