fa65de7b3dca55ea9ab0aa58f34589a638b661c9
AJ Tatum <andrew.j.tatum@gmail.com>
2017-12-20 15:26:39 -0500

    Adjusted DataMapperListConverter for arrays

--- DataFlow.Web/Helpers/DataMapperListConverter.cs
+++ DataFlow.Web/Helpers/DataMapperListConverter.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using DataFlow.Models;
 using DataFlow.Web.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -11,42 +9,65 @@ namespace DataFlow.Web.Helpers
 {
     public class DataMapperListConverter : JsonConverter
     {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(List<DataMapper>);
+        }
+
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var list = (List<DataMapper>)value;
-            var obj = new JObject(list.Select(dm =>
+            if (list.Any(dm => dm.DataMapperProperty != null))
             {
-                var val = dm.DataMapperProperty != null
-                    ? JToken.FromObject(dm.DataMapperProperty, serializer)
-                    : JToken.FromObject(dm.SubDataMappers, serializer);
-
-                return new JProperty(dm.Name, val);
-            }));
-            obj.WriteTo(writer);
+                var obj = new JObject(list.Select(dm =>
+                {
+                    var val = dm.DataMapperProperty != null
+                        ? JToken.FromObject(dm.DataMapperProperty, serializer)
+                        : JToken.FromObject(dm.SubDataMappers, serializer);
+                    return new JProperty(dm.Name, val);
+                }));
+                obj.WriteTo(writer);
+            }
+            else
+            {
+                serializer.Serialize(writer,
+                    list.Select(dm => new Dictionary<string, List<DataMapper>>
+                    {
+                        { dm.Name, dm.SubDataMappers }
+                    }));
+            }
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var token = JToken.Load(reader);
-            var list = token
-                .Children<JProperty>()
-                .Select(jp =>
-                {
-                    var mapper = new DataMapper { Name = jp.Name };
-
-                    if (jp.Value["data-type"] != null)
-                        mapper.DataMapperProperty = jp.Value.ToObject<DataMapperProperty>(serializer);
-                    else
-                        mapper.SubDataMappers = jp.Value.ToObject<List<DataMapper>>(serializer);
-                    return mapper;
-                })
-                .ToList();
-            return list;
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof(List<DataMapper>);
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    return token.Children<JProperty>()
+                        .Select(jp =>
+                        {
+                            var mapper = new DataMapper { Name = jp.Name };
+                            var val = jp.Value;
+                            if (val["data-type"] != null)
+                                mapper.DataMapperProperty = jp.Value.ToObject<DataMapperProperty>(serializer);
+                            else
+                                mapper.SubDataMappers = jp.Value.ToObject<List<DataMapper>>(serializer);
+                            return mapper;
+                        })
+                        .ToList();
+                case JTokenType.Array:
+                    return token.Children<JObject>()
+                        .SelectMany(jo => jo.Properties())
+                        .Select(jp => new DataMapper
+                        {
+                            Name = jp.Name,
+                            SubDataMappers = jp.Value.ToObject<List<DataMapper>>(serializer)
+                        })
+                        .ToList();
+                default:
+                    throw new JsonException("Unexpected token type: " + token.Type);
+            }
         }
     }
 }
