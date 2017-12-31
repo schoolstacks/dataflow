ab4954893a9a7c24521ccf1d117b8900f94ef0e2
Jason Hoekstra <jasonhoekstra@gmail.com>
2017-12-31 16:54:05 -0500

    Update ChromeExtensionController.cs
    adding new mock attribute

--- DataFlow.Web/Areas/Api/Controllers/ChromeExtensionController.cs
+++ DataFlow.Web/Areas/Api/Controllers/ChromeExtensionController.cs
@@ -1,4 +1,4 @@
-﻿using System.Net;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
@@ -28,6 +28,7 @@ namespace DataFlow.Web.Areas.Api.Controllers
 		""agent_id"": 1,
 		""action"": ""GET"",
 		""url"": ""https://school-stacks.acme.instructure.com/files/906/download?download_frd=1"",
+        ""loginUrl"": ""https://school-stacks.acme.instructure.com/login/canvas"",
 		""schedule"": [{
 			""day"": 0,
 			""hour"": 7,
@@ -38,6 +39,7 @@ namespace DataFlow.Web.Areas.Api.Controllers
 		""agent_id"": 2,
 		""action"": ""POST"",
 		""url"": ""http://moodle.schoolstacks.com/export.php"",
+        ""loginUrl": ""http://moodle.schoolstacks.com/login"",
 		""parameters"": 
 			[{""mform_isexpanded_id_gradeitems"":""1""},{""checkbox_controller1"":""1""},{""mform_isexpanded_id_options"":""0""},{""id"":""3""},{""sesskey"":""4O0S9wVEf6""},{""_qf__grade_export_form"":""1""},{""itemids[8]"":""0""},{""itemids[8]"":""1""},{""itemids[9]"":""0""},{""itemids[9]"":""1""},{""itemids[10]"":""0""},{""itemids[10]"":""1""},{""itemids[11]"":""0""},{""itemids[11]"":""1""},{""itemids[12]"":""0""},{""itemids[12]"":""1""},{""itemids[13]"":""0""},{""itemids[13]"":""1""},{""itemids[14]"":""0""},{""itemids[14]"":""1""},{""itemids[15]"":""0""},{""itemids[15]"":""1""},{""itemids[16]"":""0""},{""itemids[16]"":""1""},{""itemids[17]"":""0""},{""itemids[17]"":""1""},{""itemids[18]"":""0""},{""itemids[18]"":""1""},{""itemids[7]"":""0""},{""itemids[7]"":""1""},{""export_feedback"":""0""},{""export_onlyactive"":""0""},{""export_onlyactive"":""1""},{""display[real]"":""0""},{""display[real]"":""1""},{""display[percentage]"":""0""},{""display[letter]"":""0""},{""decimals"":""2""},{""separator"":""comma""}],
 		""schedule"": [{
